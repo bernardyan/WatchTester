19d193e711780260d0077022c2bdbb22ef63b197
BernardYan <ybn@me.com>
2016-05-11 23:26:22 -0700

    Initial Commit

---
+++ WatchTester/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WatchTester
+//
+//  Created by Bernard Yan on 5/11/16.
+//  Copyright © 2016 IntBridge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
